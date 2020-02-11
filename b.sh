79ab81ab656c6241f85b7923c470f13ecc5cb41e
Eugene Cohen <eugene@hp.com>
2020-02-11 16:03:59 -0700

    enable docker build for native_posix_64

--- b.sh
+++ b.sh
@@ -1,5 +1,12 @@
-#!/bin/zsh
+#!/bin/bash
+
+SCRIPTDIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
+cd $SCRIPTDIR
+
+export ZEPHYR_TOOLCHAIN_VARIANT=host
 
 #west build -p auto -b <your-board-name> samples/basic/blinky
 #west build -p auto -b nucleo_g431rb samples/basic/blinky
-west build -b qemu_cortex_m3 --build-dir build_qemu_m3 samples/synchronization
+#west build -p auto -b qemu_cortex_m3 --build-dir build_qemu_m3 samples/synchronization
+#west build -p auto -b native_posix --build-dir build_posix samples/synchronization
+west build -p auto -b native_posix_64 --build-dir build_posix_64 samples/synchronization
